ec96ed788f8f77b45f56a4c0f94029069d5cd62a
Rahim Remu <rahim.remu@gmail.com>
2018-06-05 18:24:56 -0400

    1st git commit

---
+++ src/ec2/vpc.py
@@ -0,0 +1,10 @@
+class VPC:
+    def __init__(self, client):
+        self._client = client
+        """:type: pyboto3.ec2"""
+
+    def create_vpc(self):
+        print('Creating A VPC....')
+        return self._client.create_vpc(
+            CidrBlock='10.0.0.0/16'
+        )
